5d706bcd9aa5de1637c5a6050e8d5c4f9c57a4b9
Diy-101 <thehorsediy101@gmail.com>
2025-07-22 16:09:30 +0300

    delete model Country

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 from os import getenv
@@ -12,6 +12,9 @@ port = getenv("DB_PORT")
 name = getenv("DB_NAME")
 
 engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}")
+
+metadata = MetaData()
+metadata.reflect(bind=engine)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
--- src/database/models.py
+++ src/database/models.py
@@ -1,10 +1,4 @@
-from src.database.connection import Base
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from src.database.connection import metadata
 
-class Country(Base):
-    __tablename__ = "country"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), index=True, nullable=False)
-    alpha_2 = Column(String(2), index=True, nullable=False)
-    alpha_3 = Column(String(3), index=True, nullable=False)
-    region = Column(String, index=True)
+country = metadata["countries"]
+
