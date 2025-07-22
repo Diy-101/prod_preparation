55ee2b330ab51831861602a35e961767c8a2cb02
Diy-101 <thehorsediy101@gmail.com>
2025-07-22 16:17:46 +0300

    add a crud function get_countries

--- src/api/country.py
+++ src/api/country.py
@@ -2,14 +2,13 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from src.schemas.country import Country
 from src.dependencies import get_db
-
+from src.database.crud import get_countries
 country_router = APIRouter()
 
 
 @country_router.get("/api/countries", tags=["start"], summary="Get all countries from database", response_model=list[Country])
-def get_countries(db: Session = Depends(get_db)):
-
-    return
+async def get_countries(db: Session = Depends(get_db)):
+    return await get_countries(db)
 
 @country_router.post("/api/countries/{alpha2}", tags=["start"], summary="Add one schemas to database")
 def add_country(name: str):
--- src/database/crud.py
+++ src/database/crud.py
@@ -1,4 +1,8 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import select
+from src.database.models import country
+
 async def get_countries(db: Session):
-    select()
+    stmt = select(country)
+    countries = db.execute(stmt).scalars().all()
+    return countries
