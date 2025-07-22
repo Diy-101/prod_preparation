4041989050f00aa05f411351754d87e53f70588f
Diy-101 <thehorsediy101@gmail.com>
2025-07-22 18:19:19 +0300

    create new tags

--- src/api/country.py
+++ src/api/country.py
@@ -6,11 +6,11 @@ from src.database.crud import select_all_countries
 country_router = APIRouter()
 
 
-@country_router.get("/api/countries", tags=["start"], summary="Get all countries from database", response_model=list[Country])
+@country_router.get("/api/countries", tags=["Country"], summary="Get all countries from database", response_model=list[Country])
 async def get_countries(db: Session = Depends(get_db)):
     result = await select_all_countries(db)
     return result
 
-@country_router.post("/api/countries/{alpha2}", tags=["start"], summary="Add one schemas to database")
+@country_router.post("/api/countries/{alpha2}", tags=["Country"], summary="Get one country from database via alpha2", response_model=list[Country])
 def add_country(name: str):
     return {"ok": True}
