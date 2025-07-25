9f9fd2dbe8469f4149adb8ffad7ba0db1991703c
Diy-101 <thehorsediy101@gmail.com>
2025-07-25 16:59:01 +0300

    some fix

--- src/country/models.py
+++ src/country/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, Integer, String
 
 class Country(Base):
     __tablename__ = "countries"
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False, unique=True)
     alpha2 = Column(String(2), nullable=False, unique=True)
--- src/database.py
+++ src/database.py
@@ -3,9 +3,6 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from src.constants import DB_URL
 
 engine = create_engine(DB_URL)
-
-metadata = MetaData()
-metadata.reflect(bind=engine)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
