730900f52d07d48b1f1aff79bb528cc88da74e88
Diy-101 <thehorsediy101@gmail.com>
2025-07-29 13:50:34 +0300

    create initial user router

--- src/user/router.py
+++ src/user/router.py
@@ -4,6 +4,6 @@ from sqlalchemy.sql.annotation import Annotated
 
 user_router = APIRouter()
 
-#@user_router.get("/users", tags=["users"], summary="Get all users")
-#async def get_all_users(token: Annotated[str, oauth2_scheme]):
-    #return {"token": token}
+@user_router.get("/users", tags=["users"], summary="Get all users")
+async def get_all_users(token: Annotated[str, oauth2_scheme]):
+    return {"token": token}
