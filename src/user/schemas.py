bacec7e89ebf6107bb91cefd3ae28061de5c3256
Diy-101 <thehorsediy101@gmail.com>
2025-07-30 16:10:50 +0300

    delete RootBase schemas and substitute them on Annotated[]

--- src/user/schemas.py
+++ src/user/schemas.py
@@ -3,23 +3,19 @@ from typing import Annotated
 from src.schemas import CountryAlpha2
 import re
 
-class UserLogin(RootModel[Annotated[str, Field(
+user_login = Annotated[str, Field(
     max_length=30,
     pattern="[a-zA-Z0-9-]+",
     examples=["yellowMonkey"],
     description="Логин пользователя"
-)]]):
-    pass
-
-class UserEmail(RootModel[Annotated[EmailStr, Field(
+)]
+user_email = Annotated[EmailStr, Field(
     max_length=50,
     min_length=1,
     examples=["yellowstone1980@you.ru"],
     description="E-mail пользователя"
-)]]):
-    pass
-
-class UserPassword(RootModel[Annotated[str, Field(
+)]
+user_password = Annotated[str, Field(
     max_length=100,
     min_length=6,
     description="""
@@ -28,45 +24,38 @@ class UserPassword(RootModel[Annotated[str, Field(
         -Присутствуют латинские символы в нижнем и верхнем регистре.
         - Присутствует минимум одна цифра.
     """
-)]]):
-    pass
-
-class UserIsPublic(RootModel[Annotated[bool, Field(
+)]
+user_is_public = Annotated[bool, Field(
     examples=["true"],
     description="Публичные профили доступны другим пользователям: если профиль публичный, \
     любой пользователь платформы сможет получить информацию о пользователе."
-)]]):
-    pass
-
-class UserPhone(RootModel[Annotated[str, Field(
+)]
+user_phone = Annotated[str, Field(
     max_length=20,
     pattern=r"\+[\d]+",
     examples=["example: +74951239922"],
     description="Номер телефона пользователя в формате +123456789"
-)]]):
-    pass
-
-class UserImage(RootModel[Annotated[HttpUrl, Field(
+)]
+user_image = Annotated[HttpUrl, Field(
     max_length=200,
     min_length=1,
     description="Ссылка на фото для аватара пользователя"
-)]]):
-    pass
+)]
 
 class UserProfile(BaseModel):
-    login: UserLogin
-    email: UserEmail
+    login: user_login
+    email: user_email
     countryCode: CountryAlpha2
-    isPublic: UserIsPublic
-    phone: UserPhone | None = None
-    image: UserImage | None = None
+    isPublic: user_is_public
+    phone: user_phone | None = None
+    image: user_image | None = None
 
     model_config = ConfigDict(
         from_attributes=True
     )
 
 class UserRegistration(UserProfile):
-    password: UserPassword
+    password: user_password
 
     @field_validator("password", mode="after")
     @classmethod
