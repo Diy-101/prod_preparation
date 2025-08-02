b8fafb9473fc94cabd991d9c402ff34054b496ee
Diy-101 <thehorsediy101@gmail.com>
2025-08-02 22:13:33 +0300

    create function for selection all users

--- src/user/utils.py
+++ src/user/utils.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from sqlalchemy import select, update
+from sqlalchemy import select
 import jwt
 
 from datetime import datetime, timedelta
@@ -19,6 +19,11 @@ TOKEN_TIME_MINUTES_EXPIRATION = auth_settings.token_time_minutes_expiration
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/sign-in")
+
+def get_users_db(db: Session = Depends(get_db)):
+    users = db.execute(select(models.User)).scalars().all()
+    users = [schemas.UserProfile.model_validate(user) for user in users]
+    return users
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -56,50 +61,4 @@ async def get_profile_via_token(token: str = Depends(oauth2_scheme), db: Session
             status_code=404,
             detail="Пользователь не найдет"
         )
-    return schemas.UserProfile(
-        login=user.login,
-        email=user.email,
-        countryCode=user.countryCode,
-        isPublic=user.isPublic,
-        phone=user.phone if user.phone else None,
-        image=user.image if user.image else None,
-    )
-
-def update_user_profile(
-        data: schemas.UserProfileUpdate,
-        token: str = Depends(oauth2_scheme),
-        db: Session = Depends(get_db)
-):
-    username = verify_token(token)
-
-    if username is None:
-        raise HTTPException(
-            status_code=401,
-            detail="Переданный токен не существует либо некорректен.",
-        )
-
-    user = db.scalars(select(models.User).where(models.User.login == username)).first()
-    if user is None:
-        raise HTTPException(
-            status_code=404,
-            detail="Пользователь не найдет"
-        )
-
-    for k, v in data.model_dump().items():
-        setattr(user, k, v)
-    db.commit()
-    db.refresh(user)
-
-    return schemas.UserProfile(
-        login=user.login,
-        email=user.email,
-        countryCode=user.countryCode,
-        isPublic=user.isPublic,
-        phone=user.phone if user.phone else None,
-        image=user.image if user.image else None,
-    )
-
-
-
-
-
+    return schemas.UserProfile.model_validate(user)
